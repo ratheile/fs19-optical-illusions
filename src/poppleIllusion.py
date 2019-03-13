c3caf82c1553773f2bfe1cb89bcf9646ec2bf892
Jonathan Kakon <jonathankakon@gmail.com>
2019-03-13 14:31:22 +0100

    - added rotational correction

--- src/poppleIllusion.py
+++ src/poppleIllusion.py
@@ -55,9 +55,13 @@ def morphCoordinates(distortion, phi):
     originalX = sin(phi)
     originalY = cos(phi)
 
-    shift = 1*(2*distortion - 1) * ((1+cos(4*phi))/2)
+    amount = 0.2
 
-    return ((originalX*(1+shift)), (originalY*(1+shift)))
+    shift = amount*(2*distortion - 1) * ((cos(4*phi)))
+
+    rotCorr = phi + 4 * amount * (2*distortion - 1) * ((sin(4*phi)))
+
+    return ((originalX*(1+shift)), (originalY*(1+shift)), rotCorr*180/pi)
 
 
 def gabor_patch(size, psi):
@@ -107,7 +111,6 @@ def draw(variationID, distortion):
 
     for i in range(patches/8 + 1):
         phi = phi0 + i * dPhi
-        phiDeg = phi * 180 / pi
         patch_mat = gabor_patch(patchsize, phi)
         # patch_mat = np.max(patch_mat)
         
@@ -117,11 +120,11 @@ def draw(variationID, distortion):
         patch = Image.open(patch_stream)
         # patch = getPatch(phi, patchsize, patchsize)
 
-        mask = Image.new('L', patch.size, 255)
-        patch = patch.rotate(phiDeg, expand=True)
-        mask = mask.rotate(phiDeg, expand=True)
-        
         coord = morphCoordinates(distortion, phi)
+        mask = Image.new('L', patch.size, 255)
+        patch = patch.rotate(coord[2], expand=True)
+        mask = mask.rotate(coord[2], expand=True)
+        
         pilImage.paste(
             patch,
             ( # the box parameter
