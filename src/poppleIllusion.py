ed8fb338dd71545ef5b355bba1d2b1c774598748
Jannis Schönleber <joennlae@gmail.com>
2019-03-26 17:19:03 +0100

    added variations

--- src/poppleIllusion.py
+++ src/poppleIllusion.py
@@ -23,7 +23,7 @@ enableCheat = False
 p = figure()
 source = ColumnDataSource()
 
-illusion_variations = {
+illusion_variations= {
     0: {"originalID": 0, "patches" : 72, 'shiftfactor': 16}, 
     1: {"originalID": 1, "patches" : 72, 'shiftfactor': 8}, 
     2: {"originalID": 2, "patches" : 40, 'shiftfactor': 16}, 
@@ -33,6 +33,18 @@ illusion_variations = {
     6: {"originalID": 6, "patches" : 96, 'shiftfactor': 16}, 
     7: {"originalID": 7, "patches" : 96, 'shiftfactor': 8}, 
     8: {"originalID": 8, "patches" : 96, 'shiftfactor': 64} 
+}
+illusion_variations_jannis= {
+    0: {"originalID": 0, "patches" : 120, 'shiftfactor': 4}, 
+    1: {"originalID": 1, "patches" : 120, 'shiftfactor': 8}, 
+    2: {"originalID": 2, "patches" : 72, 'shiftfactor': 4}, 
+    3: {"originalID": 3, "patches" : 72, 'shiftfactor': 8}, 
+    4: {"originalID": 4, "patches" : 56, 'shiftfactor': 4}, 
+    5: {"originalID": 5, "patches" : 56, 'shiftfactor': 8}, 
+    6: {"originalID": 6, "patches" : 96, 'shiftfactor': 8}, 
+    7: {"originalID": 7, "patches" : 96, 'shiftfactor': 4}, 
+    8: {"originalID": 8, "patches" : 156, 'shiftfactor': 4},
+    9: {"originalID": 9, "patches" : 156, 'shiftfactor': 8} 
 }
 
 
